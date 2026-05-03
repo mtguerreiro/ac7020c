82a92c8ae3a2192dc4e69cfe241c86660d3ed635
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2026-05-03 20:49:09 +0200

    Simplifying paths in config

--- demos/freertos_tcp_server_echo/src/Cpu0.cmake
+++ demos/freertos_tcp_server_echo/src/Cpu0.cmake
@@ -10,27 +10,27 @@ list(
 list(
     APPEND
     USER_COMPILE_SOURCES
-    "${AC7020C_COMMON}/tasks/blink.c"
-    "${AC7020C_COMMON}/ac7020c/get_uid.c"
+    "${AC7020C_ROOT}/common/tasks/blink.c"
+    "${AC7020C_ROOT}/common/ac7020c/get_uid.c"
 )
 
 list(
     APPEND
     USER_INCLUDE_DIRECTORIES
-    "${AC7020C_COMMON}/tasks"
-    "${AC7020C_COMMON}/ac7020c"
-    "${AC7020C_EXT_LIBS}"
+    "${AC7020C_ROOT}/common/tasks"
+    "${AC7020C_ROOT}/common/ac7020c"
+    "${AC7020C_ROOT}/libs"
 )
 
 list(
     APPEND
     USER_COMPILE_SOURCES
-    "${Z7_TASKS_COMMON}/netinit.c"
-    "${Z7_TASKS_COMMON}/tcpecho.c"
+    "${AC7020C_ROOT}/xil_z7_scripts/tasks/netinit.c"
+    "${AC7020C_ROOT}/xil_z7_scripts/tasks/tcpecho.c"
 )
 
 list(
     APPEND
     USER_INCLUDE_DIRECTORIES
-    "${Z7_TASKS_COMMON}"
+    "${AC7020C_ROOT}/xil_z7_scripts/tasks"
 )
